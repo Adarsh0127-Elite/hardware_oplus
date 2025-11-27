09cece9186fc6ed2a585f70a48a347c3711ab01d
NINJAHUNTER1357 <adityasingh6901012a@gmail.com>
2025-11-27 18:53:29 +0000

    Revert "Fixed keyhandler and doze compile errors for aosp based roms" This broke many sensors and proxy too This reverts commit 9a6d107ef7a8504757b7d5a5e9a99d3f871f89f1.

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -55,10 +55,7 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
             IntentFilter(AudioManager.STREAM_MUTE_CHANGED_ACTION)
         )
     }
-    // Required by new AOSP/Lineage interface
-    override fun onPocketStateChanged(inPocket: Boolean) {
-        // No-op (not used on this device)
-    }
+
     override fun handleKeyEvent(event: KeyEvent): KeyEvent? {
         if (event.action != KeyEvent.ACTION_DOWN) {
             return event
